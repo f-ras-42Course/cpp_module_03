dd89c9a43ea1eb171b1317b5878155eb02282447
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2024-05-28 14:56:26 +0200

    ex00 finished

--- ex00/include/ClapTrap.hpp
+++ ex00/include/ClapTrap.hpp
@@ -6,25 +6,30 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/27 17:01:24 by fras          #+#    #+#                 */
-/*   Updated: 2024/05/28 09:27:01 by fras          ########   odam.nl         */
+/*   Updated: 2024/05/28 14:43:32 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
 # define CLAPTRAP_HPP
 
+# include <iostream>
+
 class ClapTrap
 {
-private:
-	/* data */
-public:
-	ClapTrap();
-	ClapTrap(const ClapTrap& other);
-	~ClapTrap();
-	ClapTrap& operator=(const ClapTrap& other);
-	void attack(const std::string& target);
-	void takeDamage(unsigned int amount);	
-	void beRepaired(unsigned int amount);
+	private:
+		std::string name_;
+		int hit_points_;
+		int energy_points_;
+		int attack_damage_;
+	public:
+		ClapTrap(const std::string& name);
+		ClapTrap(const ClapTrap& other);
+		~ClapTrap();
+		ClapTrap& operator=(const ClapTrap& other);
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);	
+		void beRepaired(unsigned int amount);
 };
 
 #endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,11 +6,22 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/27 16:28:09 by fras          #+#    #+#                 */
-/*   Updated: 2024/05/28 07:22:43 by fras          ########   odam.nl         */
+/*   Updated: 2024/05/28 14:55:28 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
 int main()
 {
-	
+	ClapTrap pjotr("Pjotr");	
+	ClapTrap kees("Kees");
+	for (size_t i = 0; i < 12; i++)
+	{
+		pjotr.attack("Kees");
+	}
+	for (size_t i = 0; i < 12; i++)
+	{
+		kees.attack("Kees");
+	}
 }
